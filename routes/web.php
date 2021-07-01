a91a9c81bfd3af7a21688c7b21f32f229ef7c8db
MichaelRosa9 <michaelrosa9@gmail.com>
2021-07-01 21:57:04 +0200

    log in

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PageController@index');
 
 Auth::routes();
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function(){
+        Route::get('/', 'HomeController@index')->name('home');
+    });
 
 Route::get('/home', 'HomeController@index')->name('home');
