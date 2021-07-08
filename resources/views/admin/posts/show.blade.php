943ab8e627eff0a9bb95b01c9e3d3e2e70ca0755
MichaelRosa9 <michaelrosa9@gmail.com>
2021-07-08 17:30:38 +0200

    show tags

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,9 +6,16 @@
   <h3>
     @if ($post->category)
       Category: {{ $post->category->name }}
-  @else
-    No Category
-  @endif</h3>
+    @else
+      No Category
+    @endif
+  </h3>
+  <p>{{ $post->content }}</p>
+  <div>
+    @foreach($post->tags as $tag)
+     <span class="badge badge-primary">{{ $tag->name }}</span>
+    @endforeach
+  </div>
   <p>{{ $post->content }}</p>
   <div>
     <a href="{{ route('admin.posts.edit', $post) }}">EDIT</a>
