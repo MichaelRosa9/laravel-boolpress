0933189072dd97db33615c9bd78c23b89ad22ff4
MichaelRosa9 <michaelrosa9@gmail.com>
2021-07-13 18:08:54 +0200

    sass

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>
-    Vue!
-  </h1>
+  <div class="container">
+    <h1>
+      Vue!
+    </h1>
+  </div>
 </template>
 
 <script>
@@ -10,6 +12,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+
+  @import '../sass/frontoffice/global.scss';
 
 </style>
